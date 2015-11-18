df54b65f846d48637fe98ad9fdd609e3eefd4544
flamen <jitang.zheng@gmail.com>
2015-11-17 18:14:12 -0800

    rss

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,36 @@
+require 'rss'
+
 class HomeController < ApplicationController
   def index
   end
 
-  def baidu
+  def sitemap
 
+  end
+
+  def rss
+    xml = Rails.cache.fetch("cache://#{I18n.locale}/rss.atom", expires_in: 8.hours) do
+      rss = RSS::Maker.make('atom') do |maker|
+        maker.channel.author = Setting.site_author
+        maker.channel.updated = Time.now.to_s
+        maker.channel.about = rss_url
+        maker.channel.title = Setting.get_site_info 'title'
+
+        Cms::Article.order(id: :desc).limit(120).each do |a|
+          maker.items.new_item do |item|
+            item.link = cms_articles_url(id: a.id)
+            item.title = a.title
+            item.updated = a.updated_at
+          end
+        end
+      end
+      rss.to_s
+    end
+
+    render xml: xml
+  end
+
+  def baidu
     code = Setting.baidu_site_id
     if code == params.fetch(:id)
       render text: code
--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,3 +1,16 @@
 # RailsSettings Model
 class Setting < RailsSettings::CachedSettings
+
+  def self.set_site_info(k, v)
+    self[self._site_key(k)] = v
+  end
+
+  def self.get_site_info(k)
+    self[self._site_key(k)]
+  end
+
+  private
+  def self._site_key(k)
+    "#{I18n.locale}://site/#{k}"
+  end
 end
