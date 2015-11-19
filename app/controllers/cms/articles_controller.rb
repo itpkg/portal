f9a00715af433082066e2a3d41fa765d2777fb6a
flamen <jitang.zheng@gmail.com>
2015-11-19 12:59:48 -0800

    articles

--- app/controllers/cms/articles_controller.rb
+++ app/controllers/cms/articles_controller.rb
@@ -1,6 +1,8 @@
 class Cms::ArticlesController < ApplicationController
   layout 'cms'
 
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
     @articles = Cms::Article.select(:id, :summary, :flag, :title).where(lang: I18n.locale).order(id: :desc).page params[:page]
     @title = t 'cms.articles.index.title'
@@ -9,5 +11,65 @@ class Cms::ArticlesController < ApplicationController
   def show
     @article = Cms::Article.find params[:id]
     @article.update_column :visits, @article.visits+1
+  end
+
+  def new
+    @article = Cms::Article.new
+  end
+
+  def create
+    a = Cms::Article.new _params
+    begin
+      a.user = current_user
+      a.lang = I18n.locale
+      if a.save
+        flash[:notice] = t 'messages.success'
+        redirect_to(cms_article_path(a)) and return
+      end
+    rescue => e
+      flash[:alert] = e.to_s
+    end
+    @article = a
+    render 'new'
+  end
+
+  def edit
+    @article = Cms::Article.find params[:id]
+    unless @article.can_edit?(current_user)
+      head :forbidden
+    end
+  end
+
+  def update
+    a = Cms::Article.find params[:id]
+
+    unless a.can_edit?(current_user)
+      head :forbidden
+    end
+    begin
+      if a.update _params
+        flash[:notice] = t 'messages.success'
+        redirect_to(cms_article_path(a)) and return
+      end
+    rescue => e
+      flash[:alert] = e.to_s
+    end
+    @article = a
+    render 'edit'
+  end
+
+  def destroy
+    a = Cms::Article.find params[:id]
+    if a.can_edit?(current_user)
+      a.destroy
+      redirect_to cms_articles_path
+    else
+      head :forbidden
+    end
+  end
+
+  private
+  def _params
+    params.require(:cms_article).permit(:title, :summary, :flag, :tags, :body)
   end
 end
--- app/models/cms/article.rb
+++ app/models/cms/article.rb
@@ -4,5 +4,14 @@ class Cms::Article < ActiveRecord::Base
   belongs_to :user
   has_and_belongs_to_many :tags
 
+
+  validates :title, presence: true
+  validates :summary, presence: true
+  validates :body, presence: true
+
   enum flag: [:blog, :pictures, :video]
+
+  def can_edit?(u)
+    u && (self.user_id == u.id || u.is_admin?)
+  end
 end
