caa7312fb9752bf24e4cdda47408ea2d538f9791
flamen <jitang.zheng@gmail.com>
2015-11-21 04:05:41 -0800

    fix sitemap bug

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,5 +1,13 @@
 SitemapGenerator::Sitemap.create(default_host: "https://#{ENV['PORTAL_DOMAIN']}") do
-  Cms::Article.select(:id, :lang).order(id: :desc).each { |a| add cms_articles_path(id: a.id, locale: a.lang), changefreq: :monthly, priority: 0.7 }
-  add root_path(locale: 'en'), changefreq: :daily, priority: 0.9
-  add root_path(locale: 'zh-CN'), changefreq: :daily, priority: 0.9
+  Cms::Article.select(:id, :lang).order(id: :desc).each { |a| add cms_articles_path(a, locale: a.lang), changefreq: :monthly, priority: 0.7 }
+
+  %w(en zh-CN).each do |l|
+    add root_path(locale: l), changefreq: :daily, priority: 0.9
+    add about_us_path(locale: 1), changefreq: :daily, priority: 0.9
+
+    Cms::Tag.select(:id).each { |t| add cms_tags_path(t, locale: l), changefreq: :daily, priority: 0.7 }
+    User.select(:id).each { |u| add cms_users_path(u, locale: l), changefreq: :daily, priority: 0.2 }
+  end
+
+
 end
