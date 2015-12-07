21e9b52a789e1f8d9bb683f308294c81adb63631
flamen <jitang.zheng@gmail.com>
2015-12-07 12:30:12 -0800

    fix sitemap books bug

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -8,11 +8,12 @@ SitemapGenerator::Sitemap.create(default_host: "https://#{ENV['PORTAL_DOMAIN']}"
     add root_path(locale: l), changefreq: :daily, priority: 0.9
     add about_us_path(locale: 1), changefreq: :daily, priority: 0.9
 
+    Portal::Book.names.each {|n| add book_path(n, locale: l), changefreq: :weekly, priority: 0.5}
 
     User.select(:id).each { |u| add cms_users_path(u, locale: l), changefreq: :daily, priority: 0.2 }
   end
 
   Questionnaire::Report.select(:id).order(id: :desc).each { |r| add questionnaire_report_path(r), changefreq: :yearly, priority: 0.3 }
 
-  Portal::Book.names.each {|n| add book_path(n), changefreq: :weekly, priority: 0.5}
+
 end
