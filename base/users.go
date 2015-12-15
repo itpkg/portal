6927322d16ffdbc844680220a3a4c6ec64ade287
flamen <jitang.zheng@gmail.com>
2015-12-15 12:39:26 -0800

    users seed

--- base/users.go
+++ base/users.go
@@ -1,24 +1,31 @@
 package base
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/itpkg/portal/base/cfg"
 	"github.com/itpkg/portal/base/engine"
+	"github.com/itpkg/portal/base/utils"
 	"github.com/jinzhu/gorm"
 )
 
 type User struct {
 	Model
 
-	Uid          string    `sql:"not null;unique;type:char(36)"`
-	Logo         string    `sql:"not null"`
-	Name         string    `sql:"not null"`
-	ProviderType string    `sql:"not null;default:'email';index:idx_users_provider_type"`
-	ProviderId   string    `sql:"not null;index:idx_users_provider_id"`
+	Email        string `sql:"not null;unique"`
+	Password     string
+	Uid          string `sql:"not null;unique;type:char(36)"`
+	Logo         string `sql:"not null"`
+	Name         string `sql:"not null"`
+	ProviderType string `sql:"not null;default:'email';index:idx_users_provider_type"`
+	ProviderId   string
 	LastSignIn   time.Time `sql:"not null"`
+	SignInCount  uint      `sql:"not null;default:0"`
 
-	Roles []Role `gorm:"many2many:users_roles;"`
+	ConfirmedAt *time.Time
+	LockedAt    *time.Time
 }
 
 type Contact struct {
@@ -53,28 +60,127 @@ type Role struct {
 	Name         string `sql:"not null;index:idx_roles_name"`
 	ResourceType string `sql:"not null;default:'-';index:idx_roles_resource_type"`
 	ResourceId   uint   `sql:"not null;default:0"`
+}
 
-	Begin time.Time `sql:"not null;default:current_date;type:date"`
-	End   time.Time `sql:"not null;default:'1000-1-1';type:date"`
+type Permission struct {
+	ID     uint `gorm:"primary_key"`
+	User   User
+	UserID uint `sql:"not null"`
+	Role   Role
+	RoleID uint      `sql:"not null"`
+	Begin  time.Time `sql:"not null;default:current_date;type:date"`
+	End    time.Time `sql:"not null;default:'1000-1-1';type:date"`
+}
+
+//==============================================================================
+type Dao struct {
+}
+
+func (*Dao) NewEmailUser(tx *gorm.DB, name, email, password string) (*User, error) {
+	passwd, err := utils.Ssha512([]byte(password), 8)
+	if err != nil {
+		return nil, err
+	}
+
+	u := User{
+		Name:     name,
+		Email:    email,
+		Password: passwd,
+		Uid:      utils.Uuid(),
+	}
+	if err = tx.Create(&u).Error; err != nil {
+		return nil, err
+	}
+	return &u, nil
+}
+
+func (Dao) NewRole(tx *gorm.DB, name string, resource_type string, resource_id uint) (*Role, error) {
+	r := Role{
+		Name:         name,
+		ResourceType: resource_type,
+		ResourceId:   resource_id,
+	}
+	if err := tx.Create(&r).Error; err != nil {
+		return nil, err
+	}
+	return &r, nil
+}
+
+func (Dao) Apply(tx *gorm.DB, role uint, user uint, dur time.Duration) error {
+	begin := time.Now()
+	end := begin.Add(dur)
+	var count int
+	tx.Model(Permission{}).Where("role_id = ? AND user_id = ?", role, user).Count(&count)
+	if count == 0 {
+		return tx.Create(&Permission{
+			UserID: user,
+			RoleID: role,
+			Begin:  begin,
+			End:    end,
+		}).Error
+	} else {
+		return tx.Model(&Permission{}).Where("role_id = ? AND user_id = ?", role, user).Updates(map[string]interface{}{"begin": begin, "end": end}).Error
+	}
 }
 
 //==============================================================================
 type UsersEngine struct {
-	Db *gorm.DB `inject:"db"`
+	Db   *gorm.DB  `inject:"db"`
+	Dao  *Dao      `inject:""`
+	Http *cfg.Http `inject:""`
 }
 
 func (p *UsersEngine) Mount(*gin.Engine) {
 }
 
 func (p *UsersEngine) Seed() {
+	tx := p.Db.Begin()
+	var count int
+	tx.Model(User{}).Count(&count)
+	if count == 0 {
+		var root *User
+		var adminR *Role
+		var rootR *Role
+		var err error
+		if root, err = p.Dao.NewEmailUser(tx, "root", fmt.Sprintf("root@%s", p.Http.Domain), "changeme"); err != nil {
+			tx.Rollback()
+			return
+		}
 
+		now := time.Now()
+		dur := 24 * 365 * 10 * time.Hour
+
+		if err = tx.Model(&root).Update("confirmed_at", &now).Error; err != nil {
+			tx.Rollback()
+			return
+		}
+		if rootR, err = p.Dao.NewRole(tx, "root", "-", 0); err != nil {
+			tx.Rollback()
+			return
+		}
+		if err = p.Dao.Apply(tx, rootR.ID, root.ID, dur); err != nil {
+			tx.Rollback()
+			return
+		}
+		if adminR, err = p.Dao.NewRole(tx, "admin", "-", 0); err != nil {
+			tx.Rollback()
+			return
+		}
+		if err = p.Dao.Apply(tx, adminR.ID, root.ID, dur); err != nil {
+			tx.Rollback()
+			return
+		}
+
+	}
+	tx.Commit()
 }
 
 func (p *UsersEngine) Migrate() {
 	db := p.Db
-	db.AutoMigrate(&User{}, &Contact{}, &Role{}, &Log{})
+	db.AutoMigrate(&User{}, &Contact{}, &Role{}, &Permission{}, &Log{})
 	db.Model(&User{}).AddUniqueIndex("idx_user_provider_type_id", "provider_type", "provider_id")
-	db.Model(&Role{}).AddUniqueIndex("idx_roles_name_resource_type", "name", "resource_type")
+	db.Model(&Role{}).AddUniqueIndex("idx_roles_name_resource_type_id", "name", "resource_type", "resource_id")
+	db.Model(&Permission{}).AddUniqueIndex("idx_permissions_user_role", "user_id", "role_id")
 }
 
 //==============================================================================
--- base/utils/utils_test.go
+++ base/utils/utils_test.go
@@ -6,6 +6,30 @@ import (
 	"github.com/itpkg/portal/base/utils"
 )
 
+func TestSha(t *testing.T) {
+	s := []byte("123456")
+	if h, e := utils.Ssha512(s, 32); e == nil {
+		t.Logf("ssha512(%s) = %s", s, h)
+		if r, e := utils.Csha512(h, s); e == nil {
+			if !r {
+				t.Errorf("bad in check ssha512")
+			}
+		} else {
+			t.Errorf("bad in csha512: %v", e)
+		}
+	} else {
+		t.Errorf("bad in ssha512: %v", e)
+	}
+}
+
+func TestMd5(t *testing.T) {
+	s := "123456"
+	m := "e10adc3949ba59abbe56e057f20f883e"
+	if r := utils.Md5([]byte(s)); r != m {
+		t.Errorf("md5(%s) want %s, get %s", s, m, r)
+	}
+}
+
 func TestShell(t *testing.T) {
 	if e := utils.Shell("uname", "-a"); e != nil {
 		t.Errorf("bad in shell: %v", e)
