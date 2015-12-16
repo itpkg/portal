8861673d0652557ecd6e7394acf4d0570f3c1b1e
flamen <jitang.zheng@gmail.com>
2015-12-16 15:41:46 -0800

    fix sitemap gzip bug

--- base/seo/robots.go
+++ base/seo/robots.go
@@ -1,5 +1,11 @@
 package seo
 
+/*
+Sitemap: https://CHANGE-ME/sitemap.xml.gz
+User-agent:*
+Disallow:
+*/
+
 func Robots(txt string) (name string, body string) {
 	return "robots.txt", txt
 }
--- base/seo/sitemap/writers.go
+++ base/seo/sitemap/writers.go
@@ -7,12 +7,14 @@ import (
 )
 
 func Dump(wrt io.Writer, us *UrlSet, zip bool) error {
-	wrt.Write([]byte(xml.Header))
+
 	if zip {
 		zip := gzip.NewWriter(wrt)
 		defer zip.Close()
 		wrt = zip
 	}
+	wrt.Write([]byte(xml.Header))
 	en := xml.NewEncoder(wrt)
 	return en.Encode(us)
+
 }
